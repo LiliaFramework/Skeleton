edd492917cff50f2ddb8a47137271bfb6b2bcd3e
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-09 01:07:47 +0100

    Added Schema Sample

---
+++ schema/sh_schema.lua
@@ -0,0 +1,5 @@
+SCHEMA.name = "Sample Schema Name"
+SCHEMA.author = "Sample Schema Author"
+SCHEMA.desc = " Sample Schema Description"
+SCHEMA.version = "BETA 1.0" -- Shows the Version in the Bottom Left
+nut.util.includeDir("core", nil, true) -- LOADS THE CORE FOLDER
