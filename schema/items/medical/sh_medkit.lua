79a346b4725fecfd865f7baa4c9f293034349946
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-09 01:07:39 +0100

    Added Medical Items & Bags

---
+++ schema/items/medical/sh_medkit.lua
@@ -0,0 +1,8 @@
+ITEM.name = "Medkit"
+ITEM.desc = "Heals 50%%."
+ITEM.model = "models/weapons/w_package.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.Ticks = 0 -- Amount of Healing Ticks
+ITEM.TimePerTick = 0 -- Interval Between Each
+ITEM.HealPerTick = 0 -- How Much Each Heals
