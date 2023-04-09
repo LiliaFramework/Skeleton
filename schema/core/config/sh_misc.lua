78431f9ca9fd7fdb7eb7c1da7df869d4b3cf2e52
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-09 01:08:41 +0100

    Added Several Configs

---
+++ schema/core/config/sh_misc.lua
@@ -0,0 +1,4 @@
+SCHEMA.PlayerSpawnVehicleDelay = 90
+SCHEMA.MaximumPlayerSpawnedVehicles = 1
+nut.flag.add("C", "Access to Spawning Cars.") -- An Example Flag
+nut.currency.set("$", "Dollars", "Dollar") -- The Server Currency
