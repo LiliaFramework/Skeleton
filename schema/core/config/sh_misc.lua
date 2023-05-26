e3342aebc24274c4b696e90b0d4497d9abc3c9fb
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-26 05:54:29 +0100

    Update sh_misc.lua

--- schema/core/config/sh_misc.lua
+++ schema/core/config/sh_misc.lua
@@ -1,4 +1,3 @@
-SCHEMA.PlayerSpawnVehicleDelay = 90
 SCHEMA.MaximumPlayerSpawnedVehicles = 1
 lia.flag.add("C", "Access to Spawning Cars.") -- An Example Flag
 lia.currency.set("$", "Dollar", "Dollars") -- The Server Currency
