197036727ea23819f1420fabce6d242c45f332b9
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-29 18:42:43 +0100

    Tweaked PIM

--- skeleton/modules/pim/libs/cl_pim.lua
+++ skeleton/modules/pim/libs/cl_pim.lua
@@ -1,11 +1,10 @@
 ----------------------------------------------------------------------------------------------
+local PIM = PIM
+----------------------------------------------------------------------------------------------
 PIM_Frame = nil
 ----------------------------------------------------------------------------------------------
 function MODULE:OpenPIM()
-    if IsValid(PIM_Frame) then
-        PIM_Frame:Close()
-    end
-
+    if IsValid(PIM_Frame) then PIM_Frame:Close() end
     local frame = vgui.Create("DFrame")
     frame:SetSize(300, 120)
     frame:SetPos(0, ScrH() / 2 - frame:GetTall() / 2)
@@ -21,23 +20,11 @@ function MODULE:OpenPIM()
     end
 
     function frame:Think()
-        if not input.IsKeyDown(KEY_TAB) then
-            self:Close()
-        end
+        if not input.IsKeyDown(KEY_TAB) then self:Close() end
     end
 
     timer.Remove("PIM_Frame_Timer")
-    timer.Create(
-        "PIM_Frame_Timer",
-        30,
-        1,
-        function()
-            if frame and IsValid(frame) then
-                frame:Close()
-            end
-        end
-    )
-
+    timer.Create("PIM_Frame_Timer", 30, 1, function() if frame and IsValid(frame) then frame:Close() end end)
     frame.title = frame:Add("DLabel")
     frame.title:SetText("Player Interactions")
     frame.title:SetFont("liaSmallFont")
@@ -59,7 +46,7 @@ function MODULE:OpenPIM()
     local visibleOptionsCount = 0
     local traceEnt = LocalPlayer():GetEyeTrace().Entity
     for name, opt in pairs(PIM.options) do
-        if opt.shouldShow(LocalPlayer(), traceEnt) and traceEnt:IsPlayer() then
+        if opt.shouldShow(LocalPlayer(), traceEnt) and traceEnt:IsPlayer() and PIM:CheckDistance(LocalPlayer(), traceEnt) then
             visibleOptionsCount = visibleOptionsCount + 1
             local p = frame.list:Add("DButton")
             p:SetText(name)
@@ -75,21 +62,9 @@ function MODULE:OpenPIM()
             end
 
             function p:DoClick()
-                frame:AlphaTo(
-                    0,
-                    0.05,
-                    0,
-                    function()
-                        if frame and IsValid(frame) then
-                            frame:Close()
-                        end
-                    end
-                )
-
+                frame:AlphaTo(0, 0.05, 0, function() if frame and IsValid(frame) then frame:Close() end end)
                 opt.onRun(LocalPlayer(), traceEnt)
-                if opt.runServer then
-                    netstream.Start("PIMRunOption", name)
-                end
+                if opt.runServer then netstream.Start("PIMRunOption", name) end
             end
         end
     end
