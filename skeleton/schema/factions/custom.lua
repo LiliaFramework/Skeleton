5571055ed1aaefccd9456ab88f63e184c4e892bf
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-30 16:43:18 +0100

    Docs

--- skeleton/schema/factions/custom.lua
+++ skeleton/schema/factions/custom.lua
@@ -1,15 +1,33 @@
-﻿--------------------------------------------------------------------------------------------------------
+﻿--[[--
+    Whitelisted Faction exmaple.
+
+    This is meant to exemplify a custom faction.
+]]
+-- @configurations Faction
+
+--- Attributes of the custom faction.
+-- @realm shared
+-- @table FactionInfo
+-- @field name The name of the faction | string.
+-- @field desc The description of the faction | string.
+-- @field color The color representing the faction | Color.
+-- @field isDefault Whether the faction is set as default | boolean.
+-- @field models The models used by members of the faction | table.
+-- @field weapons The weapons available to members of the faction | table.
+
 FACTION.name = "Custom Faction"
---------------------------------------------------------------------------------------------------------
 FACTION.desc = "A Custom Faction!"
---------------------------------------------------------------------------------------------------------
 FACTION.color = Color(255, 56, 252)
---------------------------------------------------------------------------------------------------------
 FACTION.isDefault = false
---------------------------------------------------------------------------------------------------------
-FACTION.models = {"models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl", "models/Humans/Group02/male_07.mdl"}
---------------------------------------------------------------------------------------------------------
-FACTION.weapons = {"weapon_physgun", "gmod_tool"}
---------------------------------------------------------------------------------------------------------
+FACTION.models = {
+    "models/Humans/Group02/male_07.mdl",
+    "models/Humans/Group02/male_07.mdl",
+    "models/Humans/Group02/male_07.mdl",
+    "models/Humans/Group02/male_07.mdl",
+    "models/Humans/Group02/male_07.mdl"
+}
+FACTION.weapons = {
+    "weapon_physgun",
+    "gmod_tool"
+}
 FACTION_CUSTOM = FACTION.index
---------------------------------------------------------------------------------------------------------
