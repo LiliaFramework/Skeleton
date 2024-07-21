b2f9fbd6a62e212c62e3fb7199f251a088417a8c
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-21 14:02:40 +0100

    Updated Config

--- skeleton/schema/factions/custom.lua
+++ skeleton/schema/factions/custom.lua
@@ -10,22 +10,22 @@
 -- @field isDefault Whether the faction is set as default | **boolean**
 -- @field models The models used by members of the faction | **table**
 -- @field weapons The weapons available to members of the faction | **table**
--- @field pay The payment amount associated with the faction | **number**
--- @field payTimer The timer or schedule for payments | **number**
--- @field limit The limit of players in this faction at a given time | **number**
+-- @field pay The payment amount associated with the faction | **integer**
+-- @field payTimer The timer or schedule for payments | **integer**
+-- @field limit The limit of players in this faction at a given time | **integer**
 -- @field oneCharOnly Whether a player can have only one character in the faction | **boolean**
--- @field health The default health of players in the faction | **number**
--- @field armor The default armor value of players in the faction | **number**
--- @field scale The scale affecting the size of the player model | **number**
--- @field runSpeed The default running speed of players in the faction | **number**
+-- @field health The default health of players in the faction | **integer**
+-- @field armor The default armor value of players in the faction | **integer**
+-- @field scale The scale affecting the size of the player model | **float**
+-- @field runSpeed The default running speed of players in the faction | **float**
 -- @field runSpeedMultiplier Whether to multiply runSpeed by the default speed | **boolean**
--- @field walkSpeed The default walking speed of players in the faction | **number**
+-- @field walkSpeed The default walking speed of players in the faction | **float**
 -- @field walkSpeedMultiplier Whether to multiply walkSpeed by the default walk speed | **boolean**
--- @field jumpPower The default jump power of players in the faction | **number**
+-- @field jumpPower The default jump power of players in the faction | **float**
 -- @field jumpPowerMultiplier Whether to multiply jumpPower by the default jump power | **boolean**
--- @field bloodcolor The blood color of players in the faction | **number**
+-- @field bloodcolor The blood color of players in the faction | **integer**
 -- @field bodyGroups The bodygroups for player models upon spawn | **table**
--- @field index The Unique ID of the faction | **number**
+-- @field index The Unique ID of the faction | **integer**
 FACTION.name = "Custom Faction"
 FACTION.desc = "A Custom Faction!"
 FACTION.color = Color(255, 56, 252)
