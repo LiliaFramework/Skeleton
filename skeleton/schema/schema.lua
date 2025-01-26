5e25d1c1ff81243e82c8ff0f9826f84529a0e500
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-26 06:20:13 +0000

    Update schema.lua

--- skeleton/schema/schema.lua
+++ skeleton/schema/schema.lua
@@ -1,8 +1,3 @@
-﻿--[[--
-    Schema configuration options.
-
-    This is meant to define the Schema's default information.
-]]
 SCHEMA.name = "Sample Schema Name"
 SCHEMA.author = "Sample Schema Author"
 SCHEMA.desc = "Sample Schema Description"
